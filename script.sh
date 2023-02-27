bcdaf06cd92d9a037e44be46551b55c8645879d6
Juan Jesus Mamani D <jjmd.dj@gmail.com>
2023-02-27 13:04:17 -0400

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sum=0
+mkdir -p imagenezs Descargas Ecology fotozs logs programacion/{certificado,codigo/{Java/{Education,Erase,Else},Go,PHP,Python,JavaScript,Economy},texzto,Every}
+echo "hola" >> Ecology/Action.txt 
+echo "hola" >> programacion/Accept.txt
+echo "hola" >> programacion/codigo/Economy/admin.txt
+echo "hola" >> programacion/codigo/Java/Erase/aloja.txt
+echo "hola" >> programacion/Every/ala.txt
+echo "hola" >> programacion/texto/agua.txt
+echo "hola" >> programacion/texto/practica1.txt
+echo "hola" >> programacion/texto/practica2.txt
+echo "hola" >> programacion/texto/practica3.txt
+
+while IFS= read -r line
+do
+  echo "$line" >> logs/jalalog.log
+  sleep 3
+
+done < jala.txt
